f93048a7ca2db296944674fa6e957eca4fefd9b6
Lachlan Deck <lachlan.deck@gmail.com>
2022-02-15 06:19:02 +1100

    refactor Score.next to use oppositionScore and didWin boolean

--- src/main/scala/example/Score.scala
+++ src/main/scala/example/Score.scala
@@ -1,7 +1,22 @@
 package example
 
+import example.Score.{Advantage, Forty, Love}
+
 enum Score(val printVal: String):
-  def next: Option[Score] = Score.values.drop(ordinal + 1).headOption
+  def next(didWin: Boolean, opponentScore: Score): Score =
+    if (didWin) this match {
+      case Forty => if (opponentScore == Forty) Advantage else Love
+      case _     => Score.values.drop(ordinal + 1).headOption.getOrElse(Love)
+    }
+    else opponentScore match {
+      case Advantage => Love
+      case Forty => this match {
+        case Advantage => Forty
+        case Forty => this
+        case _ => Love
+      }
+      case _ => this
+    }
 
   case Love extends Score("0")
   case Fifteen extends Score("15")
--- src/test/scala/example/ScoreSpec.scala
+++ src/test/scala/example/ScoreSpec.scala
@@ -19,7 +19,7 @@ class ScoreSpec extends flatspec.AnyFlatSpec
     ("Fifteen", "15", "Thirty"),
     ("Thirty", "30", "Forty"),
     ("Forty", "40", "Advantage"),
-    ("Advantage", "Adv", null)
+    ("Advantage", "Adv", "Love")
   )
 
   forAll(scores) { (name: String, value: String, _) =>
@@ -28,16 +28,59 @@ class ScoreSpec extends flatspec.AnyFlatSpec
     }
   }
 
-  behavior of "score.next"
+  behavior of "score.next (when winning)"
 
-  forAll(scores) { (name: String, _, next: String) =>
-    if (next == null)
-      it should s"not be possible for ${name}" in {
-        Score.valueOf(name).next shouldBe empty
-      }
-    else
-      it should s"be ${name} -> ${next}" in {
-        Score.valueOf(name).next should contain(Score.valueOf(next))
-      }
+  it should "be Love -> 15" in {
+    Love.next(true, Love) shouldEqual Fifteen
+  }
+
+  it should "be 15 -> 30" in {
+    Fifteen.next(true, Love) shouldEqual Thirty
+  }
+
+  it should "be 30 -> 40" in {
+    Thirty.next(true, Love) shouldEqual Forty
+  }
+
+  it should "be 40 -> Advantage if the opponent score is 40" in {
+    Forty.next(true, Forty) shouldEqual Advantage
+  }
+
+  it should "be 40 -> Love if the opponent score is not 40" in {
+    Forty.next(true, Thirty) shouldEqual Love
+  }
+
+  it should "be Advantage -> Love" in {
+    Advantage.next(true, Forty) shouldEqual Love
+  }
+
+
+  behavior of "score.next (when losing)"
+
+  for {
+    score <- List(Love, Fifteen, Thirty, Forty)
+    oppositionScore <- List(Love, Fifteen, Thirty)
+  } {
+    it should s"not advance for ${score} against opposition score ${oppositionScore}" in {
+      score.next(false, oppositionScore) shouldEqual score
+    }
+  }
+
+  it should "remain 40 when opposition score is 40" in {
+    Forty.next(false, Forty) shouldEqual Forty
+  }
+
+  it should "be Love when opposition score was Advantage" in {
+    Forty.next(false, Advantage) shouldEqual Love
+  }
+
+  for (score <- List(Love, Fifteen, Thirty)) {
+    it should s"reset from ${score} to Love when opposition score was 40" in {
+      score.next(false, Forty) shouldEqual Love
+    }
+  }
+
+  it should "reset to 40 from Advantage" in {
+    Advantage.next(false, Forty) shouldEqual Forty
   }
 }
