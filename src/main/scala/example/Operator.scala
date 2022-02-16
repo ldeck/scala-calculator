3ebea1248c13ab1580462ec21b4765381399317b
Lachlan Deck <lachlan.deck@gmail.com>
2022-02-16 11:45:00 +1100

    add basic int maths Operators

--- src/main/scala/example/Operator.scala
+++ src/main/scala/example/Operator.scala
@@ -1,5 +1,26 @@
 package example
 
-class AddOperator {
+trait Operator:
+  def calculate(a: Int, b: Int): Int
 
+class PlusOperator extends Operator {
+  override def calculate(a: Int, b: Int): Int = a + b
 }
+
+class MinusOperator extends Operator {
+  override def calculate(a: Int, b: Int): Int = a - b
+}
+
+class TimesOperator extends Operator {
+  override def calculate(a: Int, b: Int): Int = a * b
+}
+
+class DivideOperator extends Operator {
+  override def calculate(a: Int, b: Int): Int = {
+    b match {
+      case 0 => throw IllegalArgumentException("Cannot divide by zero!")
+      case _ => a / b
+    }
+  }
+}
+
